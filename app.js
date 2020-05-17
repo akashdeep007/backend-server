a638485f6a3ac347a6859315fd52ef9ae0923af9
akashdeep007 <beingakscool@gmail.com>
2020-05-17 20:27:36 +0530

    error handling added

--- app.js
+++ app.js
@@ -19,4 +19,12 @@ app.use((req, res, next) => {
 
 app.use("/", registrationRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
 app.listen(8080);
