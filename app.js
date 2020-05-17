4d8f9ef0a72f7840969f6889d89cc6bdeaf3c39b
akashdeep007 <beingakscool@gmail.com>
2020-05-17 21:43:38 +0530

    fixes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const multer = require("multer");
+const mongoose = require("mongoose");
 
 const registrationRoutes = require("./routes/registration");
 
